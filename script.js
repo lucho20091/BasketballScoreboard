618884b4225f1d9a01529602e71c66df48b58335
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-16 14:59:44 -0500

    refactoring

--- script.js
+++ script.js
@@ -1,80 +1,37 @@
-const number1 = document.querySelector("#number1")
-const number2 = document.querySelector("#number2")
-const add1home = document.querySelector("#add1home")
-const add2home = document.querySelector("#add2home")
-const add3home = document.querySelector("#add3home")
-const add1guest = document.querySelector("#add1guest")
-const add2guest = document.querySelector("#add2guest")
-const add3guest = document.querySelector("#add3guest")
-const reset = document.querySelector("#reset")
-const leader = document.querySelector("#leader")
+const home = document.getElementById('number1');
+const guest = document.getElementById('number2');
+const leader = document.getElementById('leader');
+let countHome = 0
+let countGuest = 0
 
-let number1Count = 0
-let number2Count = 0
-let numberLeader = ""
-number1.textContent = "0"
-number2.textContent = "0"
 
-function leaderCount(){
-    if (number1Count || number2Count){
-        if (number1Count >= number2Count){
-            numberLeader = "Home is winning"
-            leader.textContent = numberLeader
-        } else {
-            numberLeader = "Guest is winning"
-            leader.textContent = numberLeader
-        }
+function add(par1, par2){
+    if (par1 === "home"){
+        countHome +=  par2
+    } else if (par1 === "guest"){
+        countGuest += par2
     }
+    home.textContent = countHome
+    guest.textContent = countGuest
+    displayTeamWinning(countHome, countGuest)
 }
 
+function displayTeamWinning(Home, Guest){
+    let message
+    if (Home > Guest){
+        message = "home is winning"
+    } else if (Guest > Home){
+        message = "guest is winning"
+    } else {
+        message = "its a tie"
+    }
+    leader.textContent = message
+}
 
-
-add1home.addEventListener("click", () => {
-    number1Count += 1
-    number1.textContent = number1Count
-    leaderCount()
-})
-
-add2home.addEventListener("click", () => {
-    number1Count += 2
-    number1.textContent = number1Count
-    leaderCount()
-
-})
-
-add3home.addEventListener("click", () => {
-    number1Count += 3
-    number1.textContent = number1Count
-    leaderCount()
-
-})
-
-add1guest.addEventListener("click", () => {
-    number2Count += 1
-    number2.textContent = number2Count
-    leaderCount()
-
-})
-
-add2guest.addEventListener("click", () => {
-    number2Count += 2
-    number2.textContent = number2Count
-    leaderCount()
-
-})
-
-add3guest.addEventListener("click", () => {
-    number2Count += 3
-    number2.textContent = number2Count
-    leaderCount()
-
-})
-
-reset.addEventListener("click", () => {
-    number1Count = 0
-    number2Count = 0
-    number1.textContent = number1Count
-    number2.textContent = number2Count
+function reset(){
+    countHome = 0
+    countGuest = 0
+    home.textContent = countHome
+    guest.textContent = countGuest
     leader.textContent = ""
-})
-
+}
